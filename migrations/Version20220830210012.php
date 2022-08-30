79bbe83248473be2e68b10bca398d5db2bc6b73e
login <assil782003@yahoo.com>
2022-08-30 23:29:20 +0200

    testing

--- migrations/Version20220830210012.php
+++ migrations/Version20220830210012.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220819080028 extends AbstractMigration
+final class Version20220830210012 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,7 +23,10 @@ final class Version20220819080028 extends AbstractMigration
         $this->addSql('CREATE TABLE boissons (id INT AUTO_INCREMENT NOT NULL, categorie_id INT NOT NULL, nom LONGTEXT NOT NULL, prix NUMERIC(4, 2) NOT NULL, photo LONGTEXT DEFAULT NULL, INDEX IDX_13E865EEBCF5E72D (categorie_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE boissons_categorie (id INT AUTO_INCREMENT NOT NULL, nom LONGTEXT NOT NULL, photo LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE carte (id INT AUTO_INCREMENT NOT NULL, categorie LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE formule (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, prix NUMERIC(4, 2) NOT NULL, photo LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE plats (id INT AUTO_INCREMENT NOT NULL, nom LONGTEXT NOT NULL, description LONGTEXT NOT NULL, prix NUMERIC(4, 2) NOT NULL, photo LONGTEXT DEFAULT NULL, categorie LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, adresse LONGTEXT NOT NULL, complement LONGTEXT DEFAULT NULL, naissance DATE NOT NULL, code_postale INT NOT NULL, ville VARCHAR(255) NOT NULL, telephone VARCHAR(255) NOT NULL, genre VARCHAR(255) NOT NULL, avatar LONGTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE boissons ADD CONSTRAINT FK_13E865EEBCF5E72D FOREIGN KEY (categorie_id) REFERENCES boissons_categorie (id)');
     }
 
@@ -34,6 +37,9 @@ final class Version20220819080028 extends AbstractMigration
         $this->addSql('DROP TABLE boissons');
         $this->addSql('DROP TABLE boissons_categorie');
         $this->addSql('DROP TABLE carte');
+        $this->addSql('DROP TABLE formule');
         $this->addSql('DROP TABLE plats');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE messenger_messages');
     }
 }
